c0172171cc73eb913433d8e74d44f70c316537b9
dagint <dagint@gmail.com>
2018-05-16 15:54:25 -0400

    got diff date working

--- shd-issues.py
+++ shd-issues.py
@@ -6,26 +6,25 @@ import decimal
 import string
 import time
 from datetime import date,datetime
+from dateutil import relativedelta,parser
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 
 #used to determine if event is related to something in SHD
 suffix = "_OPERATIONAL_ISSUE"
 
-#will serialize the date/time to make it easier to work with
-def SerializeDateTime(obj):
-    """Default JSON serializer."""
-    import calendar, datetime
+def diff_dates(date1, date2):
+    date1 = datetime.strptime(date1, "%Y-%m-%d %H:%M:%S")
+    date2 = datetime.strptime(date2, "%Y-%m-%d %H:%M:%S")
+    return abs(date2-date1).seconds
 
-    if isinstance(obj, datetime.datetime):
-        if obj.utcoffset() is not None:
-            obj = obj - obj.utcoffset()
-        millis = int(
-            calendar.timegm(obj.timetuple()) * 1000 +
-            obj.microsecond / 1000
-        )
-        return millis
-    raise TypeError('Not sure how to serialize %s' % (obj,))
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        try:
+            return super(DatetimeEncoder, obj).default(obj)
+        except TypeError:
+            return str(obj)
+
 
 # creates health object as client
 client = boto3.client('health')
@@ -38,7 +37,7 @@ response = client.describe_events (
   },
 )
 
-json_pre = json.dumps(response, default=SerializeDateTime)
+json_pre = json.dumps(response, cls=DatetimeEncoder)
 json_events = json.loads (json_pre)
 
 if (json_events['ResponseMetadata']['HTTPStatusCode']) == 200:
@@ -49,14 +48,17 @@ if (json_events['ResponseMetadata']['HTTPStatusCode']) == 200:
         if strEventTypeCode.endswith(suffix):
             strArn = (event['arn'])
             strUpdate = (event['lastUpdatedTime'])
-            print strUpdate,' ',SerializeDateTime(datetime.now()),' ',datetime.now()
+            strUpdate = parser.parse(strUpdate)
+            strUpdate = strUpdate.strftime("%Y-%m-%d %H:%M:%S")
+            now = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+            print diff_dates(strUpdate, now)
             print "%s %s" % (strArn, strUpdate)
             event_details = client.describe_event_details (
             eventArns=[
                 strArn,
                 ]
             )
-            json_event_details = json.dumps(event_details, default=SerializeDateTime)
+            json_event_details = json.dumps(event_details, cls=DatetimeEncoder)
             parsed_event_details = json.loads (json_event_details)
             #print (parsed_event_details['successfulSet'][0]['eventDescription']['latestDescription'])#print parsed_event_details
 else:
