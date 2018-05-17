d5755b9b9be7b806d1905d0fc6e66a8a660d4e3f
dagint <dagint@gmail.com>
2018-05-17 16:41:36 -0400

    more stuff

--- shd-issues.py
+++ shd-issues.py
@@ -66,83 +66,83 @@ json_pre = json.dumps(response, cls=DatetimeEncoder)
 json_events = json.loads (json_pre)
 
 if (json_events['ResponseMetadata']['HTTPStatusCode']) == 200:
-    events = json_events.get('events')
-    for event in events :
-        strEventTypeCode = event['eventTypeCode']
-        if strEventTypeCode.endswith(strSuffix):
-            strArn = (event['arn'])
-            strUpdate = (event['lastUpdatedTime'])
-            strUpdate = parser.parse(strUpdate)
-            strUpdate = strUpdate.strftime(strDTMFormat)
-            now = datetime.strftime(datetime.now(),strDTMFormat)
-            if diff_dates(strUpdate, now) < intSeconds:
-              try:
-                response = SHDIssuesTable.get_item(
-                  Key = {
-                    'arn' : strArn
-                  }
-                )
-              except ClientError as e:
-                print(e.response['Error']['Message'])
-              else:
-                #print response
-                isItemResponse = response.get('Item')
-                if isItemResponse == None:
-                  print ("record not found")
-                  response = SHDIssuesTable.put_item(
-                    Item ={
-                      'arn' : strArn,
-                      'lastUpdatedTime' : strUpdate,
-                      'added' : now,
-                      'ttl' : int(now) + int(intSeconds) + 3600
-                    }
-                  )
+  events = json_events.get('events')
+  for event in events :
+    strEventTypeCode = event['eventTypeCode']
+    if strEventTypeCode.endswith(strSuffix):
+      strArn = (event['arn'])
+      strUpdate = (event['lastUpdatedTime'])
+      strUpdate = parser.parse(strUpdate)
+      strUpdate = strUpdate.strftime(strDTMFormat)
+      now = datetime.strftime(datetime.now(),strDTMFormat)
+      if diff_dates(strUpdate, now) < intSeconds:
+        try:
+          response = SHDIssuesTable.get_item(
+            Key = {
+              'arn' : strArn
+            }
+          )
+        except ClientError as e:
+          print(e.response['Error']['Message'])
+        else:
+          #print response
+          isItemResponse = response.get('Item')
+          if isItemResponse == None:
+            print ("record not found")
+            response = SHDIssuesTable.put_item(
+              Item ={
+                'arn' : strArn,
+                'lastUpdatedTime' : strUpdate,
+                'added' : now,
+                'ttl' : int(now) + int(intSeconds) + 3600
+              }
+            )
 
-            else:
-              item = response['Item']
-              if item['lastUpdatedTime'] != strUpdate:
-                print ("last Update is different")
-                event_details = client.describe_event_details (
-                  eventArns=[
-                    strArn,
-                  ]   
-                )
-                json_event_details = json.dumps(event_details, cls=DatetimeEncoder)
-                parsed_event_details = json.loads (json_event_details)
-                healthMessage = (parsed_event_details['successfulSet'][0]['eventDescription']['latestDescription'])#print parsed_event_deta
-                statusCode = (event['statusCode'])
-                region = (event['region'])
-                eventTypeCode = (event['eventTypeCode'])
-                startTime = (event['startTime'])
-                lastUpdatedTime = (event['lastUpdatedTime'])
-                Category = (event['eventTypeCategory'])
-                Service = (event['service'])
-                eventName = str(eventTypeCode), ' - ', str(Service), ' - ', str(region)
-                healthMessage = '\n' + healthMessage + '\n\nService: ' + str(Service) + '\nRegion: ' + str(region) + '\nStatus: ' + str(statusCode)
-                print (healthMessage)
-                response = SHDIssuesTable.put_item(
-                  Item ={
-                    'arn' : strArn,
-                    'lastUpdatedTime' : strUpdate,
-                    'added' : now,
-                    'ttl' : int(now) + int(intSeconds) +  3600
-                  }
-                )
-                snsPub = snsClient.publish(
-                  Message = str(healthMessage),
-                  Subject = str(eventName),
-                  TopicArn = snsTopic
-                )
-                #print("GetItem succeeded:")
-                #print(json.dumps(item, indent=4, cls=DecimalEncoder))
-                #print len(response)
-                #bFound = False
-                #isItemResponse = response.get('Item')
-                #print isItemResponse
-                #if isItemResponse == None:
-                        #print "record not found"
+          else:
+            item = response['Item']
+            if item['lastUpdatedTime'] != strUpdate:
+              print ("last Update is different")
+              event_details = client.describe_event_details (
+                eventArns=[
+                  strArn,
+                ]   
+              )
+              json_event_details = json.dumps(event_details, cls=DatetimeEncoder)
+              parsed_event_details = json.loads (json_event_details)
+              healthMessage = (parsed_event_details['successfulSet'][0]['eventDescription']['latestDescription'])#print parsed_event_deta
+              statusCode = (event['statusCode'])
+              region = (event['region'])
+              eventTypeCode = (event['eventTypeCode'])
+              startTime = (event['startTime'])
+              lastUpdatedTime = (event['lastUpdatedTime'])
+              Category = (event['eventTypeCategory'])
+              Service = (event['service'])
+              eventName = str(eventTypeCode), ' - ', str(Service), ' - ', str(region)
+              healthMessage = '\n' + healthMessage + '\n\nService: ' + str(Service) + '\nRegion: ' + str(region) + '\nStatus: ' + str(statusCode)
+              print (healthMessage)
+              response = SHDIssuesTable.put_item(
+                Item ={
+                  'arn' : strArn,
+                  'lastUpdatedTime' : strUpdate,
+                  'added' : now,
+                  'ttl' : int(now) + int(intSeconds) +  3600
+                }
+              )
+              snsPub = snsClient.publish(
+                Message = str(healthMessage),
+                Subject = str(eventName),
+                TopicArn = snsTopic
+              )
+              #print("GetItem succeeded:")
+              #print(json.dumps(item, indent=4, cls=DecimalEncoder))
+              #print len(response)
+              #bFound = False
+              #isItemResponse = response.get('Item')
+              #print isItemResponse
+              #if isItemResponse == None:
+                #print "record not found"
                 #response = SHDIssuesTable.put_item(
-                  #  Item ={
+                  #Item ={
                               #      'arn' : strArn,
                               #      'lastUpdatedTime' : strUpdate,
                   # 'added' : now
